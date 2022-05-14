7b986a84e0a324f7d0a46967fd3253581b32e3d9
jeffsLM <jeffes.dev@gmail.com>
2022-05-14 19:31:13 -0300

    feat: login page and containers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import PaginaInicial from './components/paginaInicial/paginaInicial';
+// import PaginaInicial from './components/paginaInicial/paginaInicial';
+import { FundoSite } from './components/FundoSite';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Container } from './components/Container';
+import { LoginPage } from './pages/LoginPage'
 
 function App() {
   return (
-    <>
-    <span className='font-link'>
-      <PaginaInicial/>
-    </span>
-    </>
-    
+    <FundoSite>
+      <Header />
+      <Container>
+         <LoginPage />
+      </Container>
+      <Footer />
+    </FundoSite>
   );
 }
 

29f0e7b7886000fe3711774e3215ca73a40086ec
Jessica Vaz <jessica_vaz49@hotmail.com>
2022-05-20 20:28:34 -0300

    muda para className

--- src/components/CardLojas/Index.js
+++ src/components/CardLojas/Index.js
@@ -5,18 +5,18 @@ function CardLojas({pagTransferencia, linkImg, nomeLoja, descricao, favorito = '
     // favorito = S ou N (informar se esta favoritado com S ou N)
 
     return (
-        <a class="clica" href={pagTransferencia}>
-            <div class="card">
-                <div class="base">
-                    <img class="img" src={linkImg}></img>
+        <a className="clica" href={pagTransferencia}>
+            <div className="card">
+                <div className="base">
+                    <img className="img" src={linkImg}></img>
                 </div>
-                <div class="titulo">
-                    <span class="loja">{nomeLoja}</span>
-                    <span class={`material-icons-outlined ${favorito}`}>
+                <div className="titulo">
+                    <span className="loja">{nomeLoja}</span>
+                    <span className={`material-icons-outlined ${favorito}`}>
                         favorite
                     </span>
                 </div>
-                <span class="desc">{descricao}</span>
+                <span className="desc">{descricao}</span>
             </div>
         </a>
     );

c7654518aad3bcd1938f55410b0efdb5b99ac24a
jeffsLM <jeffes.dev@gmail.com>
2022-05-28 17:20:56 -0300

    alter: index

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import "./index.css";
 //import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Nav, Container} from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import  Botao  from '../botao/index';
 //import gif1 from '../../imagens/gif_1.gif'
 //import Botao from '../botao/index'
 
@@ -18,9 +19,10 @@ export function Header() {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="#action1">Como Funciona</Nav.Link>
-              <Nav.Link href="#action2">Acessar</Nav.Link>
-              <Nav.Link href="#action3">Abrir Conta</Nav.Link>
+              <Nav.Link className="fontColor" href="#action1">Como Funciona</Nav.Link>
+              <Nav.Link className="fontColor" href="#action2">Acessar</Nav.Link>
+              <Nav.Link className="fontColor" href="#action3">Abrir Conta</Nav.Link>
+              <Botao textoBotao="Abrir Conta" corBotao="menu-rosa"/>
             </Nav>
           </Navbar.Collapse>
         </Container>

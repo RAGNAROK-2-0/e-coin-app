de07b064f4f0c920701d63acb25b61e5edae5ff6
jefferson.matos <jefferson.matos@anchieta.br>
2022-05-31 19:53:23 -0300

    to prod

--- src/components/TransferenciaMilha/Index.js
+++ src/components/TransferenciaMilha/Index.js
@@ -1,110 +1,177 @@
-import './TransferenciaMilha.css'
-import Botao from '../botao/index'
-import Input from '../Input/Input'
-import { DataContext } from '../context/DataContext'
+import "./TransferenciaMilha.css";
+import Botao from "../Botao/index";
+import { Input } from "../Input/Input";
+import { DataContext } from "../../context/DataContext";
+import { useState, useEffect, useContext } from "react";
+import { Api } from "../../services/Api";
+import { useNavigate,useParams } from "react-router-dom";
 
 
-function TranferenciaMilhas(linkImg, nomeLoja, descLoja) {
+function TranferenciaMilhas() {
+  const navigate = useNavigate();
+  const params = useParams();
 
-    const [valorFinal, setValorFinal] = useState(0);
+  const [milhas, setMilhas] = useState();
+  const [transfere, setTransfere] = useState();
+  const [dadoApi, setDadoApi] = useState([]);
+  const [erro, setError] = useState(false);
 
-    const { Credentials } = useContext(DataContext);
+  const [cardLoja, setCardLoja] = useState([]);
 
-    let cpf;
+  const [nomeLoja, setnomeLoja] = useState("");
+  const [descLoja, setDescLoja] = useState("");
+  const [linkImg, setlinkImg] = useState("");
 
-    useEffect(() => {
-        cpf = Credentials.getCpfUser();
+  const { Credentials } = useContext(DataContext);
 
-        carregaMilhas(cpf)
-    }, []);
+  const { cpf } = Credentials.getCpfUser();
 
-    async function transfMilha(event) {
-        event.preventDefault();
+  const valorFinal = dadoApi.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposit += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraw += transaction.amount;
+        acc.total -= transaction.amount;
+      }
 
-        const data = {
-            transferencia
-        }
-
-        await transfereMilha(data);
+      return acc;
+    },
+    {
+      deposit: 0,
+      withdraw: 0,
+      total: 0
     }
+  );
 
-    async function transfereMilha({transferencia}) {
-        await Api.get('withdraw/', {
-            title: 'Transferencia de Milhas',
-            description: 'Parabéns! Você usou as suas milhas!',
-            amount: transferencia,
-            cpf: cpf
-        }).then(({ data }) => {
-            carregaMilhas(cpf)           
-        }).catch(err => {
-        })
+  useEffect(() => {
+    listaExtrato();
+    listaLojas();
+  }, []);
+
+  useEffect(
+    () => {
+     const data = cardLoja.find(
+        item => (item.idLoja == params.id)
+      );
+
+
+      console.log(cardLoja)
+      console.log(params.id)
+      console.log(data)
+
+      setnomeLoja(data?.nomeLoja);
+      setDescLoja(data?.descricaoLoja);
+      setlinkImg(data?.imgLoja);
+    },
+    [cardLoja]
+  );
+
+  async function listaLojas() {
+    await Api.get("stores")
+      .then(({ data }) => {
+        setCardLoja(data);
+      })
+      .catch(err => {});
+  }
+  async function listaExtrato() {
+    await Api.get("statement/" + cpf, {})
+      .then(({ data }) => {
+        setDadoApi(data.statement);
+      })
+      .catch(err => {});
+  }
+
+  async function transfMilha(event) {
+    event.preventDefault();
+
+    if (valorFinal < transfere) {
+      setError(true);
+    } else {
+      setError(false);
+
+      await transfereMilha();
     }
+  }
 
-    async function carregaMilhas({cpf}) {
-        await Api.get('statement/' + cpf,{
-        }).then(({ data }) => {
-            calculaValores(data.statement)
-        }).catch(err => {
-        })
-    }
+  async function transfereMilha() {
+    await Api.post("statement/withdraw", {
+      title: "Transferencia de Milhas",
+      description: "Parabéns! Você usou as suas milhas!",
+      amount: Math.abs(transfere),
+      cpf: cpf
+    })
+      .then(({ data }) => {
+        navigate("/statement");
+      })
+      .catch(err => {
+        navigate("/statement");
+      });
+  }
 
-    function calculaValores() {
-        let valorFinal = 0;
-        let valorSoma = 0;
-        let valorSub = 0;
-        dadoApi.map((item) => {
-            let amount = item.amount;
-            if (item.type === "deposit") {
-                valorSoma = valorSoma + amount;
-            } else {
-                let valor = amount.replace("-", "");
-                valorSub = valorSub - valor;
-            }
-            valorFinal = valorSoma - valorSub;
-        })
-        setValorFinal(valorFinal)
-    }
-
-    return (
-        <form onSubmit={transfMilha}>
-            <div className="campoTitulo">
-                <div className="campoTextos">
-                    <span className="transferirMilhas">Transferir Milhas</span>
-                    <span className="escolheuLoja">Agora que você escolheu a loja que quer tranferir, só falta escolher o valor.</span>
-                </div>
-                <div className="campoSvg">
-                </div>
+  return (
+    <center className="corFundo">
+      <form onSubmit={transfMilha} className="corFundo">
+        <div className="campoTitulo">
+          <div className="campoTextos">
+            <span className="transferirMilhas">Transferir Milhas</span>
+            <span className="escolheuLoja">
+              Agora que você escolheu a loja que quer tranferir, só falta
+              escolher o valor.
+            </span>
+          </div>
+          <div className="campoSvg" />
+        </div>
+        <div className="cardRosa">
+          <div className="campoTranf">
+            <div className="espacoInput campoTranf">
+              <Input
+                error={erro}
+                tipoLabel="normal"
+                titulo="Minhas milhas:"
+                estiloComponent="background"
+                type="text"
+                value={valorFinal.total + valorFinal.withdraw * 2}
+              />
             </div>
-            <div className="cardRosa">
-                <div className="campoTranf">
-                    <div className="espacoInput campoTranf">
-                        <Input tipoLabel="normal" titulo="Minhas milhas:" estiloComponent="background" tipoInput="number" value={valorFinal} onChange={e => setMilhas(e.target.value)}/>
-                    </div>
-                    <div className="espacoInput campoTranf">
-                        <Input tipoLabel="normal" titulo="Transferir:" estiloComponent="background" tipoInput="number" value={transferencia} onChange={e => setTransfere(e.target.value)}/>
-                    </div>
-                </div>
-                <div className="campoImg">
-                    <img className="seta" src="./seta-direita-reta.png"></img>
-                </div>
-                <div className="campoLoja">
-                    <div className="datalhesLoja">
-                        <div className="base">
-                            <img className="img" src={linkImg}></img>
-                        </div>
-                        <div className="campoDescricao">
-                            <span className="loja">{nomeLoja}</span>
-                            <span className="desc">{descLoja}</span>
-                        </div>
-                    </div>
-                </div>
+            <div className="espacoInput campoTranf">
+              <Input
+                error={erro}
+                tipoLabel="normal"
+                titulo="Transferir:"
+                estiloComponent="background"
+                tipoInput="number"
+                value={transfere}
+                onChange={e => setTransfere(e.target.value)}
+              />
             </div>
-            <div className="campoBtn">
-                <Botao textoBotao="Transferir" type="submmit" corBotao="verde"/>
+          </div>
+          <div className="campoImg">
+            <img className="seta" src="/seta-direita-reta.png" />
+          </div>
+          <div className="campoLoja">
+            <div className="datalhesLoja">
+              <div className="">
+                <img className="base" src={linkImg} />
+              </div>
+              <div className="campoDescricao">
+                <span className="loja">
+                  {nomeLoja}
+                </span>
+                <span className="desc">
+                  {descLoja}
+                </span>
+              </div>
             </div>
-        </form>
-    );
+          </div>
+        </div>
+        <div className="campoBtn">
+          <Botao textoBotao="Transferir" type="submmit" corBotao="verde" />
+        </div>
+      </form>
+    </center>
+  );
 }
 
-
-export {TranferenciaMilhas};
+export { TranferenciaMilhas };
